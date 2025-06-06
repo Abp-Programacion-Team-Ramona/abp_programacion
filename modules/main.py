67ffa09aa526fdf509172a41ca047186c774aa48
LuzOtero55 <marialuzoterogrion@hotmail.com>
2025-06-06 13:45:56 -0300

    subo menuadmin

--- modules/main.py
+++ modules/main.py
@@ -1,48 +1,48 @@
 from gestor_dispositivos import agregar_dispositivo, mostrar_dispositivos, buscar_dispositivo_por_nombre, \
     automatizar_dispositivo, eliminar_automatizacion, eliminar_dispositivo, mostrar_automatizaciones
 
+from gestor_usuarios import mostrar_usuarios, eliminar_usuarios, otorgar_privilegios, quitar_privilegios
 
-def menu_usuario_estandar():
-    print("Opciones:\n"
-          "1: Ver informacion de usuario\n"
-          "2: Mostrar dispositivos\n"
-          "3: Buscar dispositivo por nombre\n"
-          "4: Automatizar dispositivo\n"
-          "5: Desactivar automatizacion\n"
-          "7: Salir")
+def menu_admin():
+    print("Opciones Administrador:\n"
+          "1: Mostrar todos los dispositivos\n"
+          "2: Mostrar automatizaciones\n"
+          "3: Eliminar dispositivo\n"
+          "4: Mostrar usuarios\n"
+          "5: Eliminar usuario\n"
+          "6: Otorgar privilegios\n"
+          "7: Quitar privilegios\n"
+          "8: Salir")
 
+def ejecutar_menu_admin():
+    while True:
+        print("\n")
+        menu_admin()
+        print("\n")
+        opcion = input("Elija una opción (admin): ")
 
-aplicacion_ejecutando = True
-
-while aplicacion_ejecutando:
-
-    print("\n")
-    menu_usuario_estandar()
-    print("\n")
-
-    opcion = input("Elija una opcion:")
-
-    if opcion.isdigit():
-        opcion = int(opcion)
-        match opcion:
-            case 1:
-                agregar_dispositivo()
-            case 2:
-                mostrar_dispositivos()
-            case 3:
-                buscar_dispositivo_por_nombre()
-            case 4:
-                automatizar_dispositivo()
-            case 5:
-                eliminar_automatizacion()
-            case 6:
-                eliminar_dispositivo()
-            case 7:
-                mostrar_automatizaciones()
-            case 8:
-                print("Cerrando aplicacion.")
-                aplicacion_ejecutando = False
-            case _:
-                print("Opcion invalida.")
-    else:
-        print("Solo se admiten valores numericos")
+        if opcion.isdigit():
+            opcion = int(opcion)
+            match opcion:
+                case 1:
+                    mostrar_dispositivos()
+                case 2:
+                    mostrar_automatizaciones()
+                case 3:
+                    eliminar_dispositivo()
+                case 4:
+                    mostrar_usuarios()
+                case 5:
+                    eliminar_usuarios()
+                case 6:
+                    otorgar_privilegios()
+                case 7:
+                    quitar_privilegios()
+                case 8:
+                    print("Cerrando sesión de administrador.")
+                    break
+                case _:
+                    print("Opción inválida.")
+        else:
+            print("Solo se admiten valores numéricos.")
+            
